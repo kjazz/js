b05da9ed2cd8c949548694b8ad13b51389c6c1e4
kjazz <Krzysztof.jazgara@sonarsource.com>
2020-02-20 10:12:14 +0100

    Update source.js

--- sources/source.js
+++ sources/source.js
@@ -26,7 +26,7 @@ var Person = function(first, last, middle) { //TODO
 
 Person.prototype = { //TODO
 
-    whoAreYou : function() {
+    whoAreYou : function() { //TODO
         return this.first + (this.middle ? ' ' + 'foo' + this.middle: '') + ' ' + this.last;  //TODO
     }
 
