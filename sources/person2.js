77c44b5bb197950ffd72bc4c575dbc02932a6c24
Krzysztof Jazgara <krzysztof.jazgara@sonarsource.com>
2021-05-17 10:24:50 +0200

    fdfd

--- sources/person2.js
+++ sources/person2.js
@@ -1,14 +1,14 @@
 
-var Person = function(first, last, middle) { //TODO
-    this.first = first; //TODO
-    this.middle = middle; //TODO
+var Person2 = function(first, last, middle) {
+    this.first = first;  //TODO
+    this.middle = middle;  //TODO
     this.last = last; //TODO
 };
 
-Person.prototype = { //TODO
+Person2.prototype = {
 
-    whoAreYou : function() { //TODO
-        return this.first + (this.middle ? ' ' + 'foo' + this.middle: '') + ' ' + this.last;  //TODO
+    whoAreYou : function() {
+        return this.first + (this.middle ? ' ' + this.middle: '') + ' ' + this.last;
     }
 
 };
